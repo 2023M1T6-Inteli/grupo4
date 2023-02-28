23c13e49d7ab789953f12d23ef38e9605f39ac91
lucasnn718 <lucas.nunes@sou.inteli.edu.br>
2023-02-28 08:18:08 -0300

    Mudanças nos debuffs
    Mudei um pouco o sistema de como os debuffs acontecem para poder trabalhar melhor com eles no futuro, adicionando animações e efeitos sonoros específicos.

--- src/FirstClass/Cenas/Scripts/Debufs.gd
+++ src/FirstClass/Cenas/Scripts/Debufs.gd
@@ -19,7 +19,7 @@ func _ready():
 		$Celular/CollisionShape2D.remove_and_skip()
 		$Sono/CollisionShape2D.remove_and_skip()
 	
-		# Caso o número tenha sido o 1, a penalidade é a Bebida, por tanto a condição de cima
+		# Caso o número tenha sido o 1, a penalidade é a Bebida, portanto a condição de cima
 		# fica responsável por esconder as caixas com outros sprites sem ser a Bebida
 		# e desabilitar a colisão das outras caixas também
 		
@@ -34,7 +34,7 @@ func _ready():
 		$Celular/CollisionShape2D.remove_and_skip()
 		$Sono/CollisionShape2D.remove_and_skip()
 		
-		# Caso o número tenha sido o 2, a penalidade é a Carga pesada, por tanto 
+		# Caso o número tenha sido o 2, a penalidade é a Carga pesada, portanto 
 		# a condição de cima fica responsável por esconder as caixas com outros sprites
 		# sem ser a carga pesada e desabilitar a colisão das outras caixas também
 	
@@ -48,7 +48,7 @@ func _ready():
 		$"Carga Pesada/CollisionShape2D".remove_and_skip()
 		$Sono/CollisionShape2D.remove_and_skip()
 		
-		# Caso o número tenha sido o 3, a penalidade é o celular, por tanto 
+		# Caso o número tenha sido o 3, a penalidade é o celular, portanto 
 		# a condição de cima fica responsável por esconder as caixas com outros sprites
 		# sem ser o celular e desabilitar a colisão das outras caixas também
 		
@@ -62,47 +62,72 @@ func _ready():
 		$"Carga Pesada/CollisionShape2D".remove_and_skip()
 		$Celular/CollisionShape2D.remove_and_skip()
 		
-		# Caso o número tenha sido o 4, a penalidade é o sono, por tanto 
+		# Caso o número tenha sido o 4, a penalidade é o sono, portanto 
 		# a condição de cima fica responsável por esconder as caixas com outros sprites
 		# sem ser o sono pesada e desabilitar a colisão das outras caixas também
 
-onready var timer = $Timer as Timer
+onready var timerBebida = $Timer as Timer
 
 func _on_Bebida_area_entered(area):
-	get_parent().get_parent().get_parent().material = preload("res://Efeitos tela/ShaderBebida.tres")
-	timer.start()
+	
+		get_parent().get_parent().get_parent().material = preload("res://Efeitos tela/ShaderBebida.tres")
+		get_parent().get_parent().get_parent().get_node("AnimaçãoBebida").play("BebidaIn")
+		$TimerBebida.start()
 	
 	# Função responsável por aplicar o efeito espécial de Bebida a tela do jogador
 	
 func _on_Celular_area_entered(area):
 	Global.debuf = true
 	
-	timer.set_wait_time(1.2)
-	timer.start()
+	$TimerCelular.start()
 	
 	# Função responsável por aplicar as diferenças causadas pelo Celular ao jogador
 
 
 func _on_Carga_Pesada_area_entered(area):
 	Global.debuf2 = true
-	timer.set_wait_time(8)
-	timer.start()
+	$TimerCargaPesada.start()
 	
 	# Função responsável por aplicar as diferenças causadas pela Carga pesada ao jogador
 	
 func _on_Sono_area_entered(area):
 	get_parent().get_parent().get_parent().material = preload("res://Efeitos tela/ShaderSono.tres")
-	timer.start()
+	yield(get_tree().create_timer(1.0), "timeout")
+	get_parent().get_parent().get_parent().get_node("AnimaçãoSono").play("CenasSono")
+	$TimerSono.start()
 	
 	# Função responsável por aplicar o efeito espécial do sono a tela do jogador
 	
 
-func _on_Timer_timeout() -> void:
-	Global.debuf = false
-	Global.debuf2 = false
+
+func _on_TimerCelular_timeout() -> void:
+	
+	if Global.debuf == true:
+		Global.debuf = false
+
+	#Função responsável reverter as mudanças aplicados ao jogador após o tempo de efeito do Debuff de Celular
+
+func _on_TimerBebida_timeout():
+	
 	if get_parent().get_parent().get_parent().material == null:
 		return
-	else:
-		get_parent().get_parent().get_parent().material = null
+	elif get_parent().get_parent().get_parent().material == preload("res://Efeitos tela/ShaderBebida.tres"):
+			get_parent().get_parent().get_parent().get_node("AnimaçãoBebida").play("BebidaOut")
+
+	#Função responsável reverter as mudanças aplicados ao jogador após o tempo de efeito do Debuff de Bebida
+
+func _on_TimerCargaPesada_timeout():
 	
-	#Função responsável reverter as mudanças aplicados ao jogador após o tempo de efeito dos Debufs
+	if Global.debuf2 == true:
+		Global.debuf2 = false
+
+	#Função responsável reverter as mudanças aplicados ao jogador após o tempo de efeito do Debuff de Carga Pesada
+
+func _on_TimerSono_timeout():
+	
+	if get_parent().get_parent().get_parent().material == null:
+		return
+	elif get_parent().get_parent().get_parent().material == preload("res://Efeitos tela/ShaderSono.tres"):
+			get_parent().get_parent().get_parent().get_node("AnimaçãoSono").play("SonoOut")
+
+	#Função responsável reverter as mudanças aplicados ao jogador após o tempo de efeito do Debuff de Sono
