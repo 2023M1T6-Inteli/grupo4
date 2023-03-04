64c803a98248129584f5b38921975a00129104af
BrenoSantoS2 <breno.santos.ismart@gmail.com>
2023-03-04 08:38:27 -0300

    Tela de caminhão funcionando

--- src/FirstClass/Cenas/Scripts/Global.gd
+++ src/FirstClass/Cenas/Scripts/Global.gd
@@ -3,5 +3,6 @@ extends Node
 var debuf = false
 var debuf2 = false
 
+var cargaDoCaminhao = 0
 # Variaveis globais responsáveis por guardar a informação se o jogador está com algum dos debufs de
 # Carga ou celular ativos.
