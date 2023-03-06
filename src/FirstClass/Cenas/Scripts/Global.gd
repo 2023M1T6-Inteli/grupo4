a871bb2cbf1a6142fa745a9cc5829885ffac2f5e
rafaelarojas <rafaelacrisr@gmail.com>
2023-03-06 14:54:48 -0300

    primeira tela de interação

--- src/FirstClass/Cenas/Scripts/Global.gd
+++ src/FirstClass/Cenas/Scripts/Global.gd
@@ -6,3 +6,5 @@ var debuf2 = false
 var cargaDoCaminhao = 0
 # Variaveis globais responsáveis por guardar a informação se o jogador está com algum dos debufs de
 # Carga ou celular ativos.
+var nome = "Jorginho" #variável responsável por guardar o nome do usuário
+var nomeInstrutor #variável responsável por guarsar o nome do instritor
