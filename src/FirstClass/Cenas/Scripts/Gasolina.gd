9f676d2f7b0b426cb104c20afbf301786394e3cd
lucasnn718 <lucas.nunes@sou.inteli.edu.br>
2023-03-06 22:01:56 -0300

    Pontuação
    Implementei a pontuação e alterei pequenos detalhes sobre a gasolina.

--- src/FirstClass/Cenas/Scripts/Gasolina.gd
+++ src/FirstClass/Cenas/Scripts/Gasolina.gd
@@ -1,23 +1,11 @@
 extends Node2D
-
-#Variável responsável por armazenar a quantidade de gasolina
-var gasolina
-
-func _ready():
-	gasolina = 5000
-	set_process(true)
 	
-#Função que atualiza a variável "vida" a cada segundo
 func _process(delta):
-	yield(get_tree().create_timer(1.0), "timeout")
-	gasolina -= rand_range(1,1) #Diminuindo o valor da variável "gasolina em um valor aleatório entre 1 e 1 a cada segundo
-	if gasolina < 0:
-		gasolina = 0
-	$ProgressBar.value = gasolina #Definindo o valor da barra de progresso para o valor atual da variável "gasolina"
+	Global.gasolina -= 1 #Diminuindo o valor da variável "gasolina" em 1 a cada frame
+	if Global.gasolina < 0:
+		Global.gasolina = 0
+	$ProgressBar.value = Global.gasolina #Definindo o valor da barra de progresso para o valor atual da variável "gasolina"
 	
 	#Condição que leva para tela de Game Over caso a gasolina chegue a 0
-	if gasolina==0:
-		get_tree().change_scene("res://Cenas/Tela_Gameover.tscn")
-
-func _on_ProgressBar_ready():
-	pass # Replace with function body.
+	if Global.gasolina==0:
+		get_tree().change_scene("res://Cenas/tela_gameover.tscn")
---
+++ src/FirstClass/Cenas/Scripts/Pontuação.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+#função que mostra a pontuação do jogador
+func _process(delta):
+	$Pontos.text = str(Global.points / 12)
