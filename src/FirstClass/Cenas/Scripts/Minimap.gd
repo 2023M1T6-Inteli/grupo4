4bc0cc6e2241c463bfb9042e7a2f8573131cb49a
lucasnn718 <lucas.nunes@sou.inteli.edu.br>
2023-03-10 08:59:14 -0300

    Correção oficial bugs de exportação
    Corrigidos os bugs da exportação.

--- src/FirstClass/Cenas/Scripts/Minimap.gd
+++ src/FirstClass/Cenas/Scripts/Minimap.gd
@@ -1,16 +1,16 @@
 extends Control
 
 func _ready():
-	$"ViewportContainer/Viewport/Mapa 1/Player/Camera2D".zoom = Vector2(70,90)
+	$ViewportContainer/Viewport/Mapa1/Player/Camera2D.zoom = Vector2(70,90)
 	# Comando responsável por pegar a camera do Player e dar um zoom inverso, para que seja possível
 	# ver o mapa inteiro dentro no minimapa
 
 
 func _physics_process(delta):
-	var playerPosition = $"ViewportContainer/Viewport/Mapa 1/Player".position 
+	var playerPosition = $ViewportContainer/Viewport/Mapa1/Player.position 
 	var cursorPosition = $ViewportContainer/Viewport/cursor.position
 	
-	if $"ViewportContainer/Viewport/Mapa 1/Player".maxSpeed > 0:
+	if $ViewportContainer/Viewport/Mapa1/Player.maxSpeed > 0:
 		var tween = get_node("Tween")
 		
 		tween.interpolate_property($ViewportContainer/Viewport/cursor, 
