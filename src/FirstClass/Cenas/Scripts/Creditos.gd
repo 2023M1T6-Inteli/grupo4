c935906d09222217e24bd38e95f0f38d9d2f35c2
BrenoSantoS2 <breno.santos.ismart@gmail.com>
2023-04-04 15:58:50 -0300

    Tela de credito e Tela de dificuldade

--- src/FirstClass/Cenas/Scripts/Creditos.gd
+++ src/FirstClass/Cenas/Scripts/Creditos.gd
@@ -6,3 +6,29 @@ extends Node2D
 
 func _on_BotaoVoltar_pressed():
 	get_tree().change_scene("res://Cenas/TelaGameOver.tscn")
+
+
+func _on_LinkedinBreno():
+	OS.shell_open("https://www.linkedin.com/in/breno-santos-0843131b8/")
+
+
+func _on_LinkedinEduardo_pressed():
+	OS.shell_open("https://www.linkedin.com/in/eduardo-ferrari-aaa39b265/")
+	
+
+func _on_LinkedinKaylane_pressed():
+	OS.shell_open("https://www.linkedin.com/in/kaylanevasconcelos/")
+	
+
+func _on_LinkedinLucas_pressed():
+	OS.shell_open("https://www.linkedin.com/in/lucas-nogueira-nunes")
+		
+	
+func _on_LinkedinRafaela_pressed():
+	OS.shell_open("https://www.linkedin.com/in/rafaelarojas/")
+	
+
+func _on_LinketingRaul_pressed():
+	OS.shell_open("https://www.linkedin.com/in/raul-rezende-szpak-642079186/")
+
+
